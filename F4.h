85414f24d45e972eaf8ce2d2400c7dafe5c5d4b0
Aldegheri Alessandro <alessandroaldegherivr@gmail.com>
2023-04-26 17:31:57 +0200

    Handling * flag + various comments and little fixes Co-authored-by: Ventu <notBiji@users.noreply.github.com>

--- F4.h
+++ F4.h
@@ -10,13 +10,15 @@
 #include <signal.h>
 #include <errno.h>
 
-#define SHMINFO_KEY 1217 //shminfo
-#define SEMSYNC_KEY 2711 //semsync
-#define SEMMUTEX_KEY 7121 //semmutex
+#define SHMINFO_KEY 1217 /* Shminfo. */
+#define SEMSYNC_KEY 2711 /* Semsync. */
+#define SEMMUTEX_KEY 7121 /* Semmutex. */
 #define SECOND_SIGINT 1000
 #define MATRIX_IS_FULL 2000
 #define CLIENT_QUIT 3000
 #define RIS_DEFAULT_VALUE 4000
+#define SECONDS_BETWEEN_TURNS 1
+#define MAX_SIGINT 2
 #define MATRIX_DEFAULT_CHAR ' '
 
 union semun {
@@ -25,7 +27,7 @@ union semun {
     unsigned short * array;
 };
 
-/// @brief prints the banner on the standard output
+/// @brief Prints the banner on the standard output
 void print_banner(){
     printf("______                    ___ \n");
     printf("|  ___|                  /   |\n");
@@ -39,9 +41,10 @@ void print_banner(){
 /// @brief Converts a string into a integer
 /// @param string The string to convert
 /// @return The converted value
-int stringToInt(char * string){
+int string_to_int(char * string){
     int index = 0;
     int value = 0;
+    if (string[0]=='-')return -1;
     while (string[index]!='\0')
     {
         value *= 10;
@@ -61,13 +64,13 @@ void perror_exit_server(char *);
 
 void perror_exit_client(char *);
 
-/// @brief Given a matrix dimension and a matrix position, it calculates the shifted position in an array
+/// @brief Given a matrix dimension and a matrix coordinates, it calculates the shifted position in an array
 /// @param N Number of rows
 /// @param M Number of colums
 /// @param x The row of the position 
 /// @param y The column of the position.
 /// @return The shifted position.
-int getCoordinates(int N, int M, int x, int y){
+int get_coordinates(int N, int M, int x, int y){
     return M*x + y;
 }
 
@@ -79,7 +82,7 @@ int getCoordinates(int N, int M, int x, int y){
 void matrix_init(int * matrix, int N, int M){
     for (int i = 0; i < N; i++){
         for (int y = 0; y < M; y++){
-            matrix[getCoordinates(N, M, i, y)] = MATRIX_DEFAULT_CHAR;
+            matrix[get_coordinates(N, M, i, y)] = MATRIX_DEFAULT_CHAR;
         }
     }
 }
@@ -88,7 +91,7 @@ void matrix_init(int * matrix, int N, int M){
 /// @param matrix The matrix
 /// @param N The number of rows 
 /// @param M The number of colums
-void printMatrix(int * matrix, int N, int M){
+void print_matrix(int * matrix, int N, int M){
 
     for (int x = 0; x < M*4+1; x++){
         printf("_");
@@ -98,11 +101,11 @@ void printMatrix(int * matrix, int N, int M){
     for (int i = 0; i < N; i++){
         for (int y = 0; y < M; y++){
             if (y == M - 1){
-                printf("%c | \n", matrix[getCoordinates(N, M, i, y)]);
+                printf("%c | \n", matrix[get_coordinates(N, M, i, y)]);
             }else if(y == 0){
-                printf("| %c | ", matrix[getCoordinates(N, M, i, y)]);
+                printf("| %c | ", matrix[get_coordinates(N, M, i, y)]);
             }else{
-                printf("%c | ", matrix[getCoordinates(N, M, i, y)]);
+                printf("%c | ", matrix[get_coordinates(N, M, i, y)]);
             }
             
         }
@@ -120,7 +123,7 @@ void printMatrix(int * matrix, int N, int M){
 /// @param col The colums where to insert the symbol
 /// @param symbol The symbol to insert into the position
 /// @return 1 if the move was successful, -1 oterwhise
-int makeMove(int * matrix, int N, int M, int col, char symbol){
+int make_move(int * matrix, int N, int M, int col, char symbol){
     
     col--;
     
@@ -129,7 +132,7 @@ int makeMove(int * matrix, int N, int M, int col, char symbol){
         return -1;
     }
     int i = 0;
-    while(matrix[getCoordinates(N, M, i, col)]==MATRIX_DEFAULT_CHAR){
+    while(matrix[get_coordinates(N, M, i, col)]==MATRIX_DEFAULT_CHAR){
         i++;
     }
 
@@ -138,10 +141,8 @@ int makeMove(int * matrix, int N, int M, int col, char symbol){
         return -1;
     }
 
-    matrix[getCoordinates(N, M, i-1, col)] = symbol;
-    /*for (int i = col * M; i < N; i ++){
-        matrix[getCoordinates(N, M, i, col)] = symbol;
-    }*/
+    matrix[get_coordinates(N, M, i-1, col)] = symbol;
+
     return 1;
 }
 
@@ -156,7 +157,7 @@ int check_winner(int *matrix, int n, int m, char symbol){
     int counter = 0;
     for(int i=0; i<n; i++){
         for(int y=0; y<m; y++){
-            if(matrix[getCoordinates(n,m,i,y)]!=MATRIX_DEFAULT_CHAR)
+            if(matrix[get_coordinates(n,m,i,y)]!=MATRIX_DEFAULT_CHAR)
                 counter++;
             else
                 break;
@@ -168,7 +169,7 @@ int check_winner(int *matrix, int n, int m, char symbol){
 	/* checking the horizontal lines */
     for (int y = 0; y<m-3 ; y++ ){  //-3 because checking the 3 elements after the current
         for (int i = 0; i<n; i++){
-            if (matrix[getCoordinates(n, m, i, y)] == symbol && matrix[getCoordinates(n, m, i, y+1)] == symbol && matrix[getCoordinates(n, m, i, y+2)] == symbol && matrix[getCoordinates(n, m, i, y+3)] == symbol){
+            if (matrix[get_coordinates(n, m, i, y)] == symbol && matrix[get_coordinates(n, m, i, y+1)] == symbol && matrix[get_coordinates(n, m, i, y+2)] == symbol && matrix[get_coordinates(n, m, i, y+3)] == symbol){
                 return 1;
             }           
         }
@@ -177,7 +178,7 @@ int check_winner(int *matrix, int n, int m, char symbol){
 	/* checking the vertical lines */
     for (int i = 0; i<n-3 ; i++ ){	//well, same as above
         for (int y = 0; y<m; y++){
-            if (matrix[getCoordinates(n, m, i, y)] == symbol && matrix[getCoordinates(n, m, i+1, y)] == symbol && matrix[getCoordinates(n, m, i+2, y)] == symbol && matrix[getCoordinates(n, m, i+3, y)] == symbol){
+            if (matrix[get_coordinates(n, m, i, y)] == symbol && matrix[get_coordinates(n, m, i+1, y)] == symbol && matrix[get_coordinates(n, m, i+2, y)] == symbol && matrix[get_coordinates(n, m, i+3, y)] == symbol){
                 return 1;
             }           
         }
@@ -186,7 +187,7 @@ int check_winner(int *matrix, int n, int m, char symbol){
     /* checking the ascending diagonals (/) */
     for (int i=0; i<m-3; i++){	
         for (int y=3; y<n; y++){													
-            if (matrix[getCoordinates(n, m, y, i)] == symbol && matrix[getCoordinates(n, m, y-1, i+1)] == symbol && matrix[getCoordinates(n, m, y-2, i+2)] == symbol && matrix[getCoordinates(n, m, y-3, i+3)] == symbol){
+            if (matrix[get_coordinates(n, m, y, i)] == symbol && matrix[get_coordinates(n, m, y-1, i+1)] == symbol && matrix[get_coordinates(n, m, y-2, i+2)] == symbol && matrix[get_coordinates(n, m, y-3, i+3)] == symbol){
                 return 1;
             }
         }
@@ -195,7 +196,7 @@ int check_winner(int *matrix, int n, int m, char symbol){
     /* checking the descending diagonals (\) */
     for (int i=0; i<m-3; i++){	
         for (int y=0; y<n-3; y++){
-            if (matrix[getCoordinates(n, m, i, y)] == symbol && matrix[getCoordinates(n, m, i+1, y+1)] == symbol && matrix[getCoordinates(n, m, i+2, y+2)] == symbol && matrix[getCoordinates(n, m, i+3, y+3)] == symbol)
+            if (matrix[get_coordinates(n, m, i, y)] == symbol && matrix[get_coordinates(n, m, i+1, y+1)] == symbol && matrix[get_coordinates(n, m, i+2, y+2)] == symbol && matrix[get_coordinates(n, m, i+3, y+3)] == symbol)
                 return 1;
         }
     }
--- F4Server.c
+++ F4Server.c
@@ -9,9 +9,7 @@ int sem_mutex;
 int * matrix_pointer;
 int * shm_info_attach;
 int count_sigint = 0;
-char C1Symbol = MATRIX_DEFAULT_CHAR;
-char C2Symbol = MATRIX_DEFAULT_CHAR;
-//char symbol[2] = {MATRIX_DEFAULT_CHAR, MATRIX_DEFAULT_CHAR};
+char symbol[2] = {MATRIX_DEFAULT_CHAR, MATRIX_DEFAULT_CHAR};
 
 void delete_all(){
     shmctl(shm_matrix_id, IPC_RMID, NULL);
@@ -31,14 +29,14 @@ void perror_exit_server(char * string){
 /// @brief Handler for the SIGUSR1
 /// @param sig The value of the signal
 void sigusr1_handler(int sig){
-    if (shm_info_attach[9] == C1Symbol){  //symbol[0]
-        printf("%c is out of the game!\n", C1Symbol);  //symbol[0]
-        printf("%c is the WINNER!", C2Symbol);  //symbol[0]
+    if (shm_info_attach[9] == symbol[0]){  
+        printf("%c is out of the game!\n", symbol[0]);  
+        printf("%c is the WINNER!", symbol[1]);  
         kill(shm_info_attach[5], SIGUSR2);
     }else{
-        printf("%c is out of the game!\n", C2Symbol);  //symbol[1]
-        printf("%c is the WINNER!", C1Symbol);  //symbol[1]
-        kill(shm_info_attach[4], SIGUSR2);  //symbol[1]
+        printf("%c is out of the game!\n", symbol[1]);  
+        printf("%c is the WINNER!", symbol[0]); 
+        kill(shm_info_attach[4], SIGUSR2); 
     }
     delete_all();
     exit(0);
@@ -47,7 +45,7 @@ void sigusr1_handler(int sig){
 /// @brief Handler for the SIGINTs
 /// @param sig The value of the signal
 void sigint_handler(int sig){
-    if (++count_sigint == 2){ //end of game
+    if (++count_sigint == MAX_SIGINT){ /* End of game. */
         printf("End of Game\n");
         if (shm_info_attach != NULL){
             shm_info_attach[9] = SECOND_SIGINT;
@@ -59,11 +57,25 @@ void sigint_handler(int sig){
         }
         delete_all();
         exit(-1);
-    }else if (count_sigint == 1){ //warning
+    }else if (count_sigint == 1){ /* Warning. */
         printf("Next CTRL+C will kill the game!\n");
     }
 }
 
+void shm_info_init(int * shm_info_attach, int N, int M, int timer){
+    shm_info_attach[0]  = 0;
+    shm_info_attach[1]  = symbol[0];
+    shm_info_attach[2]  = symbol[1];
+    shm_info_attach[3]  = getpid();
+    shm_info_attach[4]  = 0;
+    shm_info_attach[5]  = 0;
+    shm_info_attach[6]  = shm_matrix_id;
+    shm_info_attach[7]  = N;
+    shm_info_attach[8]  = M;
+    shm_info_attach[9]  = RIS_DEFAULT_VALUE;
+    shm_info_attach[10] = timer;
+    shm_info_attach[11] = 0;
+}
 
 
 
@@ -83,22 +95,21 @@ int main(int argc, char *argv[])
     scanf("%d", &timer);
     
     /* Getting the info from the command line and checking if they are acceptable. */
-    const int N = stringToInt(argv[1]);
+    const int N = string_to_int(argv[1]);
     if (N < 5){
         printf("Number of Rows not acceptable...\n");
         exit(-1);
     }
-    const int M = stringToInt(argv[2]);
+    const int M = string_to_int(argv[2]);
     if (M < 5){
         printf("Number of Columns not acceptable...\n");
         exit(-1);
     }
 
-    C1Symbol = argv[3][0];
-    C2Symbol = argv[4][0];
-    //symbol[0] = argv[3][0];
-    //symbol[1] = argv[4][0];
+    symbol[0] = argv[3][0];
+    symbol[1] = argv[4][0];
 
+    /* Handling all the signals. */
     if (signal(SIGINT, sigint_handler) == SIG_ERR){
         perror("Error Handling CTRL+C!\n");
         exit(-1);
@@ -109,24 +120,23 @@ int main(int argc, char *argv[])
         exit(-1);
     }
     
-
     /* Sops for the sync semaphores. */
     struct sembuf sops[3];
-    /* For blocking myself. */
+    /* For locking myself. */
     sops[0].sem_num = 2;
-    sops[0].sem_op = -2; // subtract 1 from server semaphore
+    sops[0].sem_op  = -2; /* Subtract 1 from server semaphore. */
     sops[0].sem_flg = 0;
-    /* For unblocking the first client. */
+    /* For unlocking the first client. */
     sops[1].sem_num = 0;
-    sops[1].sem_op = +1; // add 1 from client1 semaphore
+    sops[1].sem_op  = +1; /* Add 1 from client1 semaphore. */
     sops[1].sem_flg = 0;
-    /* For unblocking the second client. */
+    /* For unlocking the second client. */
     sops[2].sem_num = 1;
-    sops[2].sem_op = +1; // add 1 from client2 semaphore
+    sops[2].sem_op  = +1; /* Add 1 from client2 semaphore. */
     sops[2].sem_flg = 0;
 
     /* Creating the shm that store the matrix. */
-    if ((shm_matrix_id = shmget(IPC_PRIVATE, sizeof(int) * N * M, IPC_CREAT | 0777)) == -1){
+    if ((shm_matrix_id = shmget(IPC_PRIVATE, sizeof(int) * N * M, IPC_CREAT | 0777 | IPC_EXCL)) == -1){
         perror_exit_server("Matrix Shared Memory...");
     }
 
@@ -138,10 +148,10 @@ int main(int argc, char *argv[])
     /* Initializing the matrix. */
     matrix_init(matrix_pointer, N, M);
 
-    printf("Inizializzata la matrice...\n");
+    printf("Initializing matrix...\n");
 
     /* Creating the shm that store various information. */
-    if ((shm_info = shmget(SHMINFO_KEY, sizeof(int) * 12, IPC_CREAT | 0777)) == -1){
+    if ((shm_info = shmget(SHMINFO_KEY, sizeof(int) * 12, IPC_CREAT | 0777 | IPC_EXCL)) == -1){
         perror_exit_server("Info Shared Memory...");
     }
 
@@ -151,31 +161,18 @@ int main(int argc, char *argv[])
     }
 
     /* Putting info into the shm. */
-    shm_info_attach[0] = 0;
-    shm_info_attach[1] = C1Symbol;
-    //shm_info_attach[1] = symbol[0];
-    shm_info_attach[2] = C2Symbol;
-    //shm_info_attach[2] = symbol[1];
-    shm_info_attach[3] = getpid();
-    shm_info_attach[4] = 0;
-    shm_info_attach[5] = 0;
-    shm_info_attach[6] = shm_matrix_id;
-    shm_info_attach[7] = N;
-    shm_info_attach[8] = M;
-    shm_info_attach[9] = RIS_DEFAULT_VALUE;
-    shm_info_attach[10] = timer;
-    shm_info_attach[11] = 0;
+    shm_info_init(shm_info_attach, N, M, timer);
 
-    printf("Inizializzata le info...\n");
+    printf("Initializing infos...\n");
 
     /* Creating the sem sync semaphore. */
     if ((sem_sync = semget(SEMSYNC_KEY, 3, IPC_CREAT | IPC_EXCL | 0777))== -1){
         perror_exit_server("Semaphore Sync...");
     }
 
-    /* Union for setting al the semaphores previosly created. */
+    /* Union for setting all the semaphores previosly created. */
     union semun arg;
-    short values[3] = {0, 0, 0};
+    unsigned short values[3] = {0, 0, 0};
     arg.array = values;
 
     /* Setting all the semsync semaphores. */
@@ -183,7 +180,7 @@ int main(int argc, char *argv[])
         perror_exit_server("Assigning Sem Sync...");
     }
 
-    printf("Settato il semsync...\n");
+    printf("Setting semsync...\n");
 
     /* Setting the mutex semaphore. */
     if ((sem_mutex = semget(SEMMUTEX_KEY, 1, IPC_CREAT | IPC_EXCL | 0777))== -1)
@@ -194,14 +191,14 @@ int main(int argc, char *argv[])
         perror_exit_server("Assigning Sem Mutex...");
     }
 
-    printf("Settato il semmutex...\n");
-    printf("Mi blocco...\n");
+    printf("Setting semmutex...\n");
+    printf("Waiting for players...\n");
 
     if ((semop(sem_sync, &sops[0], 1)) == -1){
-        perror_exit_server("Blocking Server...");
+        perror_exit_server("Locking Server...");
     }
     
-    printf("Mi sono sbloccato, dovrebbero essere arrivati i client: %d %d %d\n", getpid(), shm_info_attach[4], shm_info_attach[5]);
+    /* Declarations. */
     int turn;
     int win;
     
@@ -210,43 +207,20 @@ int main(int argc, char *argv[])
         sops[0].sem_op = -1;
         shm_info_attach[11] = 0;
         while(1){
-
-            printf("Vai Client %d\n", (turn%2) + 1);
-            /* Ublocking the client corresponding to the current turn. */
+            /* Ulocking the client corresponding to the current turn. */
             if (semop(sem_sync, &sops[(turn % 2) + 1], 1) == -1)
                 perror_exit_server("Error waking the current client...");
 
-            /* Blocking myself and handling if a signal make the semop fails. */
+            /* Locking myself and handling if a signal make the semop fails. */
             if (semop(sem_sync, &sops[0], 1) == -1){
                 if (errno == EINTR){
                     if (semop(sem_sync, &sops[0], 1) == -1)
                         break;
                 }
                 else
-                    perror_exit_server("Error blocking myself...");
+                    perror_exit_server("Error locking myself...");
             }
-
-            /* gonna remove this and replacing it with the one commented below. */
-            if (!(turn%2)){  // client 0
-                win = check_winner(matrix_pointer, N, M, C1Symbol);
-                if ( win == 1){
-                    printf("Partita terminata! Vince: %c\n", C1Symbol);
-                    break;
-                }else if(win == -1){
-                    printf("Matrice Piena\n");
-                    break;
-                }
-            }else {  //client 1
-                win = check_winner(matrix_pointer, N, M, C2Symbol);
-                if (win == 1){
-                    printf("Partita terminata! Vince: %c\n", C2Symbol);
-                    break;
-                }else if(win == -1){
-                    printf("Matrice Piena\n");
-                    break;
-                }
-            }
-            /*
+            /* Checking if either client has won. */
             win = check_winner(matrix_pointer, N, M, symbol[turn%2]);
             if ( win == 1){
                 printf("Game over! The winner is: %c\n", symbol[turn%2]);
@@ -255,42 +229,38 @@ int main(int argc, char *argv[])
                 printf("The matrix is full!\n");
                 break;
             }
-            */
+            
             turn ++;
         }
 
         if (win == -1)
             shm_info_attach[9] = MATRIX_IS_FULL;
-        else{
-            if (!(turn%2))
-                shm_info_attach[9] = C1Symbol;
-            else 
-                shm_info_attach[9] = C2Symbol;
-            //shm_info_attach[9] = symbol[turn%2];
-            
-        }
+        else
+            shm_info_attach[9] = symbol[turn%2];
+        
         kill(shm_info_attach[4], SIGUSR1);
         kill(shm_info_attach[5], SIGUSR1);
 
-        printf("Waiting for clients...\n");
+        printf("Waiting for clients' decision...\n");
 
         sops[0].sem_op = -2;
 
-        /* Blocking myself. */
+        /* Locking myself. */
         if ((semop(sem_sync, &sops[0], 1)) == -1){
 
             if (errno == EINTR){
 
                 if (semop(sem_sync, &sops[0], 1) == -1){
-                    perror_exit_server("Blocking Server...\n");
+                    perror_exit_server("Locking Server...\n");
                 }
 
             }else{
-                perror_exit_server("Blocking Server...");
+                perror_exit_server("Locking Server...");
             }
             
         }
         
+        /* Handling if one player doesn't wanna play anymore. */
         if(shm_info_attach[11] != 2){
             shm_info_attach[9] = CLIENT_QUIT;
             kill(shm_info_attach[4], SIGUSR1);
@@ -305,8 +275,6 @@ int main(int argc, char *argv[])
 
     printf("Game Over!\n");
     
-    
-
     delete_all();
     return 0;
 }
